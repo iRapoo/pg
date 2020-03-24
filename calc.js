218ebc490be6b867f13268ec4085e552d5e28d4b
iRapoo <rapoo.git@gmail.com>
2020-03-24 11:03:20 +0300

    Если минут больше 0 добавляем разницу от времени перезагрузке и расчетной

--- calc.js
+++ calc.js
@@ -125,6 +125,8 @@ function time() {
 function timeConvert(sec) {
     let h = sec / 3600 ^ 0;
     let m = (sec - h * 3600) / 60 ^ 0;
+    let bal = m % KD;
+    m = (bal > 0) ? m + (KD - bal) : m;
     return ((h < 10 ? '0' + h : h) + ' ч. ' + (m < 10 ? '0' + m : m) + ' мин. ');
 }
 
