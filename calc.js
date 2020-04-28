752b569b6972af84596e989ba17098cbcd14ba87
iRapoo <rapoo.git@gmail.com>
2020-04-28 10:54:32 +0300

    При достижении и привышении удалять отметку времени из памяти

--- calc.js
+++ calc.js
@@ -80,6 +80,7 @@ function render() {
     } else {
         RESULT.html(resObject.message);
         ADD_BTN.hide();
+        localStorage.removeItem('timePosition');
         alarm('stop');
     }
     localStorage.setItem('savedHeap', JSON.stringify(HEAP));
